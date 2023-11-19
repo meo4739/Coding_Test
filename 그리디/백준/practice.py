0874220b053aeed8361bbf7f6148854a8bc622aa
song <meo4739@naver.com>
2023-11-19 15:02:47 +0900

    coding

---
+++ 그리디/백준/practice.py
@@ -0,0 +1,5 @@
+a = int(1)
+b = int(2)
+c = 0
+c += a-b
+print(c)
--- 그리디/백준/주식_송주영_11_12.py
+++ 그리디/백준/주식_송주영_11_12.py
@@ -1,3 +1,25 @@
 import sys
 sys.stdin = open('./그리디/백준/주식.txt')
-print(int(input()))
+
+d = int(input())
+for _ in range(d):
+    n = int(input()) # 날의 수를 입력받는다.
+    prices = list(map(int, input().split() )) 
+    print('prices', prices)
+    max_profit = 0
+    max_price = 0
+    
+    for price in reversed(prices):
+        print('\n반복문 시작')
+        print('price', price)
+        if price > max_price:
+            print('max_pirce 업데이트')
+            max_price = price
+            print('max_profit', max_profit)
+            print('max_price', max_price)
+        else:
+            print('max_profit 업데이트')
+            max_profit += max_price - price
+            print('max_profit', max_profit)
+            print('max_price', max_price)
+    print(max_profit)
