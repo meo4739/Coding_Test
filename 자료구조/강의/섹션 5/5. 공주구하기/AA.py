ee4e3c35d572a1df234ebf503739c187bfd2398b
song <meo4739@naver.com>
2023-11-26 15:16:58 +0900

    coding

--- 자료구조/강의/섹션 5/5. 공주구하기/AA.py
+++ 자료구조/강의/섹션 5/5. 공주구하기/AA.py
@@ -1,4 +1,18 @@
 import sys
+from collections import deque
+
 sys.stdin = open('./자료구조/강의/섹션 5/5. 공주구하기/input.txt')
 input = sys.stdin.readline
 
+n, k = map(int, input().split())
+
+dq = list(range(1, n+1))
+# print(dq)
+dq = deque(dq)
+
+while dq:
+    for _ in range(k-1):
+        cur = dq.popleft()
+        dq.append(cur)
+    dq.popleft()
+    
